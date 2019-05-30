b5675c1cd16270f411b2ee4c301ef4fe4227b967
rg777777 <rg777777@gmail.com>
2019-05-31 01:00:52 +0400

    initial

---
+++ config/index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  REDIS: {
+    PORT: process.env.REDIS_PORT,
+    HOST: process.env.REDIS_HOST,
+    PASSWORD: process.env.REDIS_PASS,
+    PROJECT_QUEUE: 'PROJECTQUEUE'
+  }
+}
